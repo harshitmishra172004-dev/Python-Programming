087ee05183369daa0aa24927192f32b9e7880553
Harshit Mishra <harshitmishra172004@gmail.com>
2026-07-29 11:11:22 +0530

    Remove index validation from character removal script

--- remove_character_at_an_index.py
+++ remove_character_at_an_index.py
@@ -1,9 +1,4 @@
 name = "Harshit Mishra"
 index = (int(input("Enter the index of the character you want to remove: ")))
 
-if index < 0 or index >= len(name):
-    print("Invalid index. Please enter a valid index.")
-
-else:
-    new_name = name[:index] + name[index + 1:]
-    print("New string after removing character at index", index, ":", new_name)
+print(name[:index] + name[index+1:])
