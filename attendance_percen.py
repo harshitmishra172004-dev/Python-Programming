c72740c2113103ae7e118bdb71b3e9a8f4bc6783
Harshit Mishra <harshitmishra172004@gmail.com>
2026-07-30 15:03:27 +0530

    Add script to calculate attendance percentage of present employees using a loop

---
+++ attendance_percen.py
@@ -0,0 +1,19 @@
+#To calculate attendence percentage of present employees , using a loop with 'continue'to skip absent-marked records.
+
+total_employees = int(input("Enter total number of employees: "))
+
+present = 0
+
+for i in range(1, total_employees + 1):
+    status = input(f"Employee {i} attendance (P/A): ").upper()
+
+    if status == "A":
+        continue   # Skip absent employees
+
+    present += 1
+
+attendance_percentage = (present / total_employees) * 100
+
+print("\nTotal Employees :", total_employees)
+print("Present Employees :", present)
+print("Attendance Percentage : {:.2f}%".format(attendance_percentage))
