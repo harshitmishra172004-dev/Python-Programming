fc454d6b304529ce287fd6932be67c714642bd78
Harshit Mishra <harshitmishra172004@gmail.com>
2026-09-01 13:52:42 +0530

    Add script to find the Fibonacci series using recursion

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+#WAP to find the fibonacci series using recursion and take a user input.
+def fibo(n):
+    if n<=1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+num = int(input("Enter the number of terms: "))
+print("Fibonacci sequence:")
+for i in range(num):
+    print(fibo(i), end=" ")
